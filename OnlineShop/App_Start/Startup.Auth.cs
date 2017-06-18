50411ee53d0cf47625656e818aca0775c9fc0fb9
unknown <wanbi2893tuan@gmail.com>
2017-06-18 21:24:59 +0700

    chuc nang load danh sach san pham

--- OnlineShop/App_Start/Startup.Auth.cs
+++ OnlineShop/App_Start/Startup.Auth.cs
@@ -11,6 +11,7 @@ namespace OnlineShop
 {
     public partial class Startup
     {
+        //them choi
         // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
         public void ConfigureAuth(IAppBuilder app)
         {
